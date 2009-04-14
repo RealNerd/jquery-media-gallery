27a0cf862ea01dee037973f1c4758a9ecfb0a627
blake.schwendiman <blake.schwendiman@c61390fe-293d-11de-b896-015b45f1b0e6>
2009-04-14 23:32:12 +0000

    First real working copy

--- jquery.media_gallery-1.0.0.js
+++ jquery.media_gallery-1.0.0.js
@@ -14,9 +14,40 @@
  *
  */
 
-/** Usage
+/** Usage:
  * 
  * $(selector).media_gallery(options)
+ *
+ * media_gallery uses a simple HTML structure with an image and
+ * a media file to create a clickable gallery that selects a media file
+ * based on an image (likely a thumbnail)
+ *
+ * the structure of the HTML should be similar to:
+ * <div id="media_gallery"></div> <!-- you must have a target container for the media somewhere on your page -->
+ * 
+ * <div class="media_gallery"> <!-- outer container, selector for plugin -->
+ *   <img src="http://img.youtube.com/vi/i5LcBdbH8bo/2.jpg"> <!-- thumbnail should be at this level -->
+ *   <div> <!-- media container, optional parameter to media_gallery -->
+ *     <object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/oHg5SJYRHA0&hl=en&fs=1"></param>
+ *       <param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param>
+ *       <embed src="http://www.youtube.com/v/oHg5SJYRHA0&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed>
+ *     </object>
+ *   </div>
+ * </div>
+ *
+ * Options:
+ *   target_container:  the element representing the target container
+ *                      default is '#media_gallery'
+ *                      
+ *   media_container:   the element type of the media container
+ *                      default is 'div'
+ *                      
+ *   auto_select_first: should media_gallery automatically display the first
+ *                      media item?
+ *                      default is 'true'
+ *
+ * Examples:
+ *   $('.media_gallery').media_gallery(); // default usage
  *
  */
 
@@ -25,21 +56,33 @@
   // plugin definition
   //
   $.fn.media_gallery = function(options) {
-        // build main options before element iteration
-        var opts = $.extend({}, $.fn.media_gallery.defaults, options);
+    // build main options before element iteration
+    var opts = $.extend({}, $.fn.media_gallery.defaults, options);
+    var cur_item = 0;
+    
+    return this.each(function() {
+      $this = $(this);
+      // build element specific options
+      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
 
-        return this.each(function() {
-          $this = $(this);
-          // build element specific options
-          var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+      $(o.media_container, $this).hide();
+      $(o.thumbnail_element, $this).bind('click', function() {
+        $(o.target_container).html($(o.media_container, $(this).parent()).html());
+      }).css({'cursor': 'pointer'});
 
-        });
+      // select first media item if applicable      
+      if (o.auto_select_first && cur_item == 0) {
+        $(o.target_container).html($(o.media_container, $this).html());
+      }
+      cur_item++;
+    });
   };
 
-  //
   // plugin defaults
-  //
   $.fn.media_gallery.defaults = {
-        target: '#media_gallery'
+    target_container: '#media_gallery',
+    media_container: 'div',
+    auto_select_first: true,
+    thumbnail_element: 'img'
   };
 })(jQuery);
